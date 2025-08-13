262a048674f377bf2a4ce8735716a633f236b50f
szmergon <k4nin-sergei@yandex.ru>
2025-08-13 22:12:12 +0300

    modal layout added

--- script.js
+++ script.js
@@ -84,3 +84,29 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   }).mount();
 });
+
+
+export function openModal(modal) {
+  document.addEventListener('keydown', closeModalByEscape);
+  modal.classList.add('popup_is-opened');
+}
+
+export function closeModal(modal) {
+  document.removeEventListener('keydown', closeModalByEscape);
+  modal.classList.remove('popup_is-opened');
+}
+
+function closeModalByEscape(event) {
+  if (event.key === 'Escape') {
+    const openPopup = document.querySelector('.popup.popup_is-opened');
+    if (openPopup) {
+      closeModal(openPopup);
+    }
+  }
+} 
+
+export function handleCloseModalByClick(evt) {
+  if (evt.target === evt.currentTarget || evt.target.classList.contains('popup__close')) {
+    closeModal(evt.currentTarget);
+  }
+}
